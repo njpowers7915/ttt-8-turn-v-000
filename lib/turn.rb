1e5e15413ba298d7c24d0150e31828ee376ff79e
Nicholas Powers <njpowers7915@gmail.com>
2018-05-02 18:05:33 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,42 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i - 1
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return false
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) == true && position_taken?(board, index) == false
+    return true
+  else
+    return false
+  end
+end
+
+def move(board, index, character)
+  board[index] = character
+  return board
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets
+  input_to_index(user_input)
+  valid_move?(board, index)
+    if 
+end
