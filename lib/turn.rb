a138bc91ca1456a339e7f553d0283699c3e27d3e
Nicholas Powers <njpowers7915@gmail.com>
2018-05-02 18:47:39 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,6 +39,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
     move(board, index, character = "X")
+    puts board
   else
     turn(board)
   end
